72dce0d17a80cd51fa46480fe145a5da87754d1e
Al Proulx <alain.proulx@hotmail.com>
2020-03-11 10:48:47 -0400

    Modified to fallback to calculated muni

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import json
 from datetime import datetime
 import base64
 
+with open('config.json') as config_json:
+    data = json.load(config_json)
+
+fallbackMuniTable = data['fallbackMuniTable']
+
 
 def getInsertValue(value):
     returnVal = ''
@@ -19,7 +24,8 @@ def getInsertValue(value):
 
 
 def insertFeature(row, feature):
-    type = row['type'].encode('utf-8')
+    # itemType = row['type'].encode('utf-8').title()
+    itemType = row['type']
     typeId = row['type_id']
     priority = row['priority']
     nameField = row['field_name']
@@ -48,16 +54,43 @@ def insertFeature(row, feature):
             connWeblive, pointSql.format(geometryGeojson))
         pointGeojson = pointRow['geojson']
 
-    name = feature['properties'][nameField].encode('utf-8').title()
-    muni = feature['properties'][muniField].encode('utf-8').title()
-    locationString = name + "|" + type + "|" + muni
-    locationId = base64.b64encode(locationString)
+    # name = feature['properties'][nameField].encode('utf-8').title()
+    # muni = feature['properties'][muniField].encode('utf-8').title()
+    name = feature['properties'][nameField]
+    if (name == None):
+        return "OK"
 
+    # SELECT muni FROM public.sc_simcoectyjurisdictions where ST_Intersects(geom, ST_SetSRID(ST_GeomFromGeoJSON('{"type": "Point", "coordinates": [-8929107.2899, 5543302.053]}'), 3857));
+    # GET MUNI IF WE DON'T HAVE ONE
+    if (muniField == None and fallbackMuniTable != ""):
+        muniSql = "SELECT muni FROM {0} where ST_Intersects(geom, ST_SetSRID(ST_GeomFromGeoJSON('{1}'), 3857));"
+        muniRow = postgres.queryOne(
+            connWeblive, muniSql.format(fallbackMuniTable, pointGeojson))
+        if (muniRow == None):
+            muni = ''
+        else:
+            muni = muniRow['muni']
+            if (muni == None):
+                muni = ''
+    else:
+        if (muniField == None):
+            muni = ''
+        else:
+            muni = feature['properties'][muniField]
+            if (muni == None):
+                muni = ''
+
+    locationString = name + \
+        "|" + itemType + \
+        "|" + muni
+    #locationId = base64.b64encode(locationString.encode('utf-8').title())
+    locationIdBytes = base64.b64encode(locationString.encode('utf-8'))
+    locationId = str(locationIdBytes, "utf-8")
     if (aliasField == None):
         alias = None
     else:
         if feature['properties'][aliasField] != None:
-            alias = feature['properties'][aliasField].encode('utf-8').title()
+            alias = feature['properties'][aliasField]
         else:
             alias = None
 
@@ -67,9 +100,9 @@ def insertFeature(row, feature):
     # print(type)
     # print(muni)
     values = (getInsertValue(name), getInsertValue(alias),
-              getInsertValue(type), typeId, getInsertValue(muni), geometryGeojson, extentGeojson, pointGeojson, locationId, priority)
-    return postgres.executeNonQuery(
-        connTabular, insertSqlTemplate, values)
+              getInsertValue(itemType), typeId, getInsertValue(muni), geometryGeojson, extentGeojson, pointGeojson, locationId, priority)
+    a = postgres.executeNonQuery(connTabular, insertSqlTemplate, values)
+    return a
 
 
 def isNumber(value):
@@ -80,14 +113,21 @@ def isNumber(value):
         return False
 
 
+def dropIndex(conn, command):
+    try:
+        postgres.executeNonQuery(connTabular, command)
+        return True
+    except:
+        return False
+
+
 connTabular = postgres.getConn('tabular')
 connWeblive = postgres.getConn('weblive')
 
 # DROP INDEX
-postgres.executeNonQuery(connTabular, "DROP INDEX tbl_search_trgm_idx_name;")
-postgres.executeNonQuery(connTabular, "DROP INDEX tbl_search_trgm_idx_alias;")
-postgres.executeNonQuery(
-    connTabular, "DROP INDEX tbl_search_trgm_idx_priority;")
+dropIndex(connTabular, "DROP INDEX IF EXISTS tbl_search_trgm_idx_name;")
+dropIndex(connTabular, "DROP INDEX IF EXISTS tbl_search_trgm_idx_alias;")
+dropIndex(connTabular, "DROP INDEX IF EXISTS tbl_search_trgm_idx_priority;")
 
 rows = postgres.query(connTabular, 'SELECT id, "type", type_id, field_name, field_name_alias, muni_field_name, roll_number_field, run_schedule, priority, last_run, last_run_minutes,wfs_url FROM public.tbl_search_layers;')
 for row in rows:
@@ -95,7 +135,7 @@ for row in rows:
     startTime = datetime.now()
 
     # GET VALUES
-    type = row['type']
+    itemType = row['type']
     typeId = row['type_id']
     nameField = row['field_name']
     muniField = row['muni_field_name']
@@ -103,10 +143,11 @@ for row in rows:
     wfsUrl = row['wfs_url']
     runSchedule = row['run_schedule']
 
-    if (runSchedule == None):
+    # if (runSchedule != 'ALWAYS'):
+    if (runSchedule != 'ONETIME'):
         continue
 
-    print("Processing: " + type + " " + typeId)
+    print("Processing: " + itemType + " " + typeId)
 
     typeId2 = (typeId)
     postgres.executeNonQuery(
